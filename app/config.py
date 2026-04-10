def34bd9e318a0c922d92315a375142413f1f51f
RishabhRawat12 <rishbahrawatgeu@gmail.com>
2026-04-10 20:31:40 +0530

    updated project

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL must be set in environment")
